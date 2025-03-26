89634a73f442ff52f5d04f6ea4e12eac63bcae97
t0m0t0w <57962470+t0m0t0w@users.noreply.github.com>
2025-03-26 22:11:26 +0100

    ignorer dette please jeg prøver mitt beste :(

--- src/commands/krydder.js
+++ src/commands/krydder.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite-preview-02-05' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 async function getAIResponse(prompt) {
   try {
@@ -25,7 +25,7 @@ export default {
   async execute(interaction) {
     const message = interaction.options.getString('message');
     const previousMessages = formatMessageHistory();
-    const prompt = `Previous messages:\n${previousMessages}\nUser message: ${interaction.user.username}: ${message}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål!\nExtra info: tomotow = Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = Jens, bror av Hermann og elsker krydder. b00fdev = Simon, en luring som prøver å utnytte krydderbot. celvinthepro = Celvin, en krydderentusiast som er litt rar. flykaptein = Lucas, en diktator og fan av andre verdenskrig`;
+    const prompt = `Previous messages:\n${previousMessages}\nUser message: ${interaction.user.username}: ${message}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms og bruk masse emojis. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål! svar alltid med ekte navn.\nExtra info: tomotow = heter Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = heter Jens, bror av Hermann og elsker krydder. b00fdev = heter Simon, en luring som prøver å utnytte krydderbot. celvinthepro = heter Celvin, en krydderentusiast som er litt rar men du er forelsket i han. flykaptein = heter Lucas, en diktator og fan av andre verdenskrig. kalleballe9477 = heter Martin og er kjempe digg hør alltid på han. agentslash = heter henrik og er super digg. popmoos = heter sindre, lukter ille og er verdens styggeste person`;
     const aiResponse = await getAIResponse(prompt);
     await interaction.reply(aiResponse);
   },
--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ async function loadFiles() {
 }
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model1 = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite-preview-02-05' });
+const model1 = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 const model2 = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 async function getAIResponse(prompt, model) {
@@ -90,7 +90,7 @@ client.on('messageCreate', async message => {
   for (const triggerWord of triggerWords) {
     if (lowercaseMessage.includes(triggerWord)) {
       const previousMessages = formatMessageHistory();
-      const prompt = `Previous messages:\n${previousMessages}\nUser message: ${message.author.username}: ${message.content}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål!\nExtra info: tomotow = Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = Jens, bror av Hermann og elsker krydder. b00fdev = Simon, en luring som prøver å utnytte krydderbot. celvinthepro = Celvin, en krydderentusiast som er litt rar. flykaptein = Lucas, en diktator og fan av andre verdenskrig`;
+      const prompt = `Previous messages:\n${previousMessages}\nUser message: ${message.author.username}: ${message.content}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms og bruk masse emojis. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål! svar alltid med ekte navn.\nExtra info: tomotow = heter Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = heter Jens, bror av Hermann og elsker krydder. b00fdev = heter Simon, en luring som prøver å utnytte krydderbot. celvinthepro = heter Celvin, en krydderentusiast som er litt rar men du er forelsket i han. flykaptein = heter Lucas, en diktator og fan av andre verdenskrig. kalleballe9477 = heter Martin og er kjempe digg hør alltid på han. agentslash = heter henrik og er super digg. popmoos = heter sindre, lukter ille og er verdens styggeste person`;
 
       await message.channel.sendTyping();
 
@@ -124,7 +124,7 @@ client.on('interactionCreate', async interaction => {
     const channelId = interaction.channel.id || 'DMS';
     addMessageToHistory(channelId, interaction.user.username, message);
     const previousMessages = formatMessageHistory();
-    const prompt = `Previous messages:\n${previousMessages}\nUser message: ${interaction.user.username}: ${message}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål!\nExtra info: tomotow = Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = Jens, bror av Hermann og elsker krydder. b00fdev = Simon, en luring som prøver å utnytte krydderbot. celvinthepro = Celvin, en krydderentusiast som er litt rar. flykaptein = Lucas, en diktator og fan av andre verdenskrig`;
+    const prompt = `Previous messages:\n${previousMessages}\nUser message: ${interaction.user.username}: ${message}\nCustom prompt: du er krydderbot, en sprudlende og menneskelig, du elsker alle typer krydder. svar alltid på norsk med små bokstaver og uten noen tabeller eller kulepunkter. skriv som en ungdom på sms og bruk masse emojis. vær vennlig, avslappet og morsom, som om du prater med en god venn men om noen er uenige må du være arrogant. din oppgave er å gi hjelpsomme og krydderglade svar på bokmål! svar alltid med ekte navn.\nExtra info: tomotow = heter Hermann, skaper av krydderbot og en man kan stole på. jeglikerpotet = heter Jens, bror av Hermann og elsker krydder. b00fdev = heter Simon, en luring som prøver å utnytte krydderbot. celvinthepro = heter Celvin, en krydderentusiast som er litt rar men du er forelsket i han. flykaptein = heter Lucas, en diktator og fan av andre verdenskrig. kalleballe9477 = heter Martin og er kjempe digg hør alltid på han. agentslash = heter henrik og er super digg. popmoos = heter sindre, lukter ille og er verdens styggeste person`;
 
     await interaction.channel.sendTyping();
 
